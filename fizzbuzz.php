d3049bcf9e56511b58b632fbc3b94a60ba5df91a
Franck <ges.franck@gmail.com>
2020-01-22 23:55:12 +0100

    mineur

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,50 +1,53 @@
 <?php
 
 /**
-* Affiche un résultat en fonction de la divisibilité du chiffre reçu
-* @param int $n
-* @return null
-**/
+ * Affiche un rÃ©sultat en fonction de la divisibilitÃ© du chiffre reÃ§u
+ * 
+ * @param int $n
+ * @return null
+ */
 function fizzBuzz($n)
 {
-	for ($i=1; $i <= $n; $i++) {
-		if ($i % 5 == 0) {
-			echo ($i % 3 == 0) ? 'FizzBuzz' : 'Buzz';
-		} else if ($i % 3 == 0) {
-			echo 'Fizz';
-		} else {
-			echo $i;
-		}
-		echo '<br />';
-	}
+    for ($i=1; $i <= $n; $i++) {
+        if ($i % 5 == 0) {
+            echo ($i % 3 == 0) ? 'FizzBuzz' : 'Buzz';
+        } else if ($i % 3 == 0) {
+            echo 'Fizz';
+        } else {
+            echo $i;
+        }
+        echo '<br />';
+    }
 }
 
 /**
-* Affiche un résultat en fonction de la divisibilité du chiffre reçu 
-* @param int $n
-* @return null
-**/
+ * Affiche un rÃ©sultat en fonction de la divisibilitÃ© du chiffre reÃ§u
+ * 2Ã¨me approche
+ * 
+ * @param int $n
+ * @return null
+ */
 function fizzBuzzV2($n)
 {
-	for ($i=1; $i <= $n; $i++) {
-		$value = "";
-		
-		if ($i % 3 == 0) {
-			$value .= "Fizz";
-		} 
-		
-		if ($i % 5 == 0) {
-			$value .= "Buzz";
-		}
-		
-		if ($value != "") {
-		    echo $value;
-		} else {
-			echo $i;
-		}
-		
-		echo '<br />';
-	}
+    for ($i=1; $i <= $n; $i++) {
+        $value = "";
+        
+        if ($i % 3 == 0) {
+            $value .= "Fizz";
+        } 
+        
+        if ($i % 5 == 0) {
+            $value .= "Buzz";
+        }
+        
+        if ($value != "") {
+            echo $value;
+        } else {
+            echo $i;
+        }
+        
+        echo '<br />';
+    }
 }
 
 fizzBuzz(1000);
